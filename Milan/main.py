cf975d6458006c49b904d8fabb72d874c1d97390
milantokic <89388259+milantokic@users.noreply.github.com>
2021-08-30 09:27:45 +0200

    Adding word2vec model

--- Milan/main.py
+++ Milan/main.py
@@ -1,25 +1,54 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import NearestNeighbors
+import nltk
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+import gensim.downloader
 
+nltk.download('wordnet')
+nltk.download('punkt')
 
 def read_data(path):
-    corpus = pd.read_csv(path, sep='\t')
-    return corpus
+    cr = pd.read_csv(path, sep='\t')
+    return cr
+
+
+lem = WordNetLemmatizer()
+wv = gensim.downloader.load('word2vec-google-news-300')
+
+
+def lemmatize_sent(sent):
+    lem_sent = []
+    for word in word_tokenize(sent):
+        if word.isalnum():
+            lem_sent.append(lem.lemmatize(word))
+    return " ".join(lem_sent)
+
+
+def sent_w2v(sent):
+    vector_list = []
+    for token in word_tokenize(lemmatize_sent(sent)):
+        try:
+            vector_list.append(wv[token])
+        except:
+            pass
+    return vector_list
+
+
+def sum_vectors(vector_list):
+    return np.sum(vector_list, axis=0)
 
 
 corpus = read_data('insurance_qna_dataset.csv').iloc[:, 1:]
 corpus = corpus.groupby('Question', as_index=False).agg(lambda x: np.unique(x).tolist())
 corpus_q = corpus['Question']
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(corpus_q)
-
-print('Enter new question')
+all_vec = []
+print("Input question: ")
 new_q = input()
-N = 10
-vector_new = vectorizer.transform([new_q])
-# find 10 nearest neighbors using ball tree algorithm
-nbrs = NearestNeighbors(n_neighbors=N, algorithm='kd_tree').fit(X)
-print(corpus_q.iloc[nbrs.kneighbors(vector_new)[1].flatten()])
+
+all_sim = [cosine_similarity([sum_vectors(sent_w2v(new_q))] , [sum_vectors(sent_w2v(corpus_q[i]))]) for i in range(corpus_q.shape[0])]
+
+top_10 = np.argsort(all_sim, axis=None)[-10:][::-1]
+
