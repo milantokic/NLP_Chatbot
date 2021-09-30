b61b477a498c4c283d418696e404d6d999418147
milantokic <89388259+milantokic@users.noreply.github.com>
2021-09-30 22:59:28 +0200

    add idf component (#15)

--- Milan/Lemmatization_KDTree_IDF.py
+++ Milan/Lemmatization_KDTree_IDF.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 import nltk
-from nltk.stem import PorterStemmer
+from sklearn.neighbors import NearestNeighbors
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import word_tokenize
 import gensim.downloader
-from sklearn.neighbors import NearestNeighbors
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download('punkt')
 
@@ -15,37 +15,39 @@ def read_data(path):
     return cr
 
 
-corpus = read_data('insurance_qna_dataset.csv').iloc[:, 1:]
-corpus = corpus.groupby('Question', as_index=False).agg(lambda x: np.unique(x).tolist())
-corpus_q = corpus['Question']
-
-vectorizer = TfidfVectorizer(token_pattern='(?u)\\b\\w+\\b')
-X = vectorizer.fit_transform(corpus_q)
-
-porter = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 wv = gensim.downloader.load('word2vec-google-news-300')
 
 
-def stem_sent(sent):
-    lem_sent = []
+def lem_sent(sent):
+    lem_sentence = []
     for word in word_tokenize(sent):
         if word.isalnum():
-            lem_sent.append(porter.stem(word))
-    return " ".join(lem_sent)
+            lem_sentence.append(lemmatizer.lemmatize(word))
+    return " ".join(lem_sentence)
 
 
 def sent_w2v(sent):
     vector_list = []
-    for token in word_tokenize(stem_sent(sent)):
+    for token in word_tokenize(lem_sent(sent)):
         try:
             vector_list.append(wv[token])
         except:
-            vector_list.append(np.zeros(300))
+            pass
     return vector_list
 
 
-def average_vectors(vector_list):
-    return np.mean(vector_list, axis=0)
+def sum_vectors(vector_list):
+    return np.sum(vector_list, axis=0)
+
+
+corpus = read_data('insurance_qna_dataset.csv').iloc[:, 1:]
+corpus = corpus.groupby('Question', as_index=False).agg(lambda x: np.unique(x).tolist())
+corpus_q = corpus['Question']
+
+
+vectorizer = TfidfVectorizer(token_pattern='(?u)\\b\\w+\\b')
+X = vectorizer.fit_transform(corpus_q)
 
 
 idf_dict = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
@@ -70,17 +72,15 @@ def add_idf(sent):
 all_vec = []
 for i in range(corpus_q.shape[0]):
     try:
-        all_vec.append(average_vectors(add_idf(corpus_q[i])))
+        all_vec.append(sum_vectors(add_idf(corpus_q[i])))
     except:
         pass
-all_vec = np.array(all_vec)
-model = NearestNeighbors(n_neighbors=25, algorithm='ball_tree').fit(all_vec)
+model = NearestNeighbors(n_neighbors=25, algorithm='kd_tree').fit(all_vec)
 
 print("Input question: ")
-
 new_q = input()
 
-new_vec = [average_vectors(add_idf(new_q))]
+new_vec = [sum_vectors(add_idf(new_q))]
 
 sim_questions = model.kneighbors(new_vec)
 print(corpus_q[sim_questions[1].flatten()])
